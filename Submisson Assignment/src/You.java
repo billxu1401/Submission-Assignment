2b5d9fa10b7d732a8d35bafdcb33e80cfcdaadd3
billxu1401 <34110282+billxu1401@users.noreply.github.com>
2019-06-06 13:25:19 -0400

    Added constructors and methods
    :)

--- Submisson Assignment/src/You.java
+++ Submisson Assignment/src/You.java
@@ -25,6 +25,29 @@ public class You{
 	private String favoriteAnimal;
 	private int age;
 
-	// Write your code here
+	public You()
+	{
+		firstName = "Bill";
+		lastName = "Xu";
+		favoriteAnimal = "Dog";
+		age = 17;
+	}
 	
+	public String getFullName()
+	{
+		return firstName + " " + lastName;
+	}
+	
+	public String setFavoriteAnimal(String animal)
+	{
+		favoriteAnimal = animal;
+		return favoriteAnimal;
+	}
+	
+	public int happyBirthday()
+	{
+		System.out.println("Happy Birthday!");
+		age++;
+		return age;
+	}
 }
